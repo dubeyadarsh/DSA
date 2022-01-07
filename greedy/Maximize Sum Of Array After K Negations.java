22c0d0cccae014baaf34a432379e87154647c195
Adarsh Dubey <75479397+dubeyadarsh@users.noreply.github.com>
2022-01-07 16:00:07 +0530

    Create Maximize Sum Of Array After K Negations.java

---
+++ greedy/Maximize Sum Of Array After K Negations.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int largestSumAfterKNegations(int[] nums, int k) {
+      Arrays.sort(nums);
+        int ans=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]<0 && k>0){
+                nums[i]=-nums[i];
+                k--;
+            }
+            ans+=nums[i];
+        }
+        if(k>0){
+            Arrays.sort(nums);
+            if((k&1)!=0) ans-=2*nums[0];
+        }
+        return ans;
+    }
+}

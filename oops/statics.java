b5b39a5966700a989a4fc5006a66b9e36f193200
Adarsh Dubey <75479397+dubeyadarsh@users.noreply.github.com>
2022-01-27 16:12:24 +0530

    Update statics.java

--- oops/statics.java
+++ oops/statics.java
@@ -1,7 +1,14 @@
 //  The static member belongs to the class, not to objects of that class. i.e, when u create a variable static it will be globally constatnt
 //  that means if u assigned some value in first object then it will effect the value in 2nd object as well.
 
+// The static keyword is a non-access modifier in Java that is applicable for the following: 
 
+// Blocks
+// Variables
+// Methods
+// Classes
+
+// *******************static variables***************************
 
 package oops;
 
@@ -28,3 +35,93 @@ class person {
     }
 }
 //Output --> Aur loading again ram 1 
+
+// **********static block***********
+// If you need to do the computation in order to initialize your static variables,
+// you can declare a static block that gets executed exactly once, when the class is first loaded. 
+
+  
+class Test
+{
+    // static variable
+    static int a = 10;
+    static int b;
+      
+    // static block
+    static {
+        System.out.println("Static block initialized.");
+        b = a * 4;
+    }
+  
+    public static void main(String[] args)
+    {
+       System.out.println("from main");
+       System.out.println("Value of a : "+a);
+       System.out.println("Value of b : "+b);
+    }
+}
+// Output:-->
+// Static block initialized.
+// from main
+// Value of a : 10
+// Value of b : 40
+
+// *********static method************
+// Use the static variable for the property that is common to all objects.
+//     For example, in class Student, all students share the same college name. Use static methods for changing static variables.
+
+// Student class
+class Student {
+    String name;
+    int rollNo;
+  
+    // static variable
+    static String cllgName;
+  
+    // static counter to set unique roll no
+    static int counter = 0;
+  
+    public Student(String name)
+    {
+        this.name = name;
+  
+        this.rollNo = setRollNo();
+    }
+  
+    // getting unique rollNo
+    // through static variable(counter)
+    static int setRollNo()
+    {
+        counter++;
+        return counter;
+    }
+  
+    // static method
+    static void setCllg(String name) { cllgName = name; }
+  
+    // instance method
+    void getStudentInfo()
+    {
+        System.out.println("name : " + this.name);
+        System.out.println("rollNo : " + this.rollNo);
+  
+        // accessing static variable
+        System.out.println("cllgName : " + cllgName);
+    }
+}
+  
+// Driver class
+public class StaticDemo {
+    public static void main(String[] args)
+    {
+        // calling static method
+        // without instantiating Student class
+        Student.setCllg("XYZ");
+  
+        Student s1 = new Student("Alice");
+        Student s2 = new Student("Bob");
+  
+        s1.getStudentInfo();
+        s2.getStudentInfo();
+    }
+}

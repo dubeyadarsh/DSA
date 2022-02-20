621c0d0c44a14535bace805ad9a9516d238cb446
Adarsh Dubey <75479397+dubeyadarsh@users.noreply.github.com>
2022-02-20 14:31:54 +0530

    Create FindPivotInO(logN).java

---
+++ hackWithInfy/FindPivotInO(logN).java
@@ -0,0 +1,21 @@
+// The main idea for finding pivot is – for a sorted (in increasing order) and pivoted array,
+// pivot element is the only element for which next element to it is smaller than it.
+
+ static int findPivot(int arr[], int low, int high)
+    {
+        // base cases
+        if (high < low)
+            return -1;
+        if (high == low)
+            return low;
+ 
+        /* low + (high - low)/2; */
+        int mid = (low + high) / 2;
+        if (mid < high && arr[mid] > arr[mid + 1])
+            return mid;
+        if (mid > low && arr[mid] < arr[mid - 1])
+            return (mid - 1);
+        if (arr[low] >= arr[mid])
+            return findPivot(arr, low, mid - 1);
+        return findPivot(arr, mid + 1, high);
+    }

d6587dc655fe3c278e1171937e0a7b73ad9093b9
Adarsh Dubey <75479397+dubeyadarsh@users.noreply.github.com>
2022-06-24 16:15:24 +0530

    Update LongestPallindromicSubseq.java

--- dp/LongestPallindromicSubseq.java
+++ dp/LongestPallindromicSubseq.java
@@ -1,3 +1,6 @@
 // String s is given aur hume s ka longest pallindromic subseq nikalna hai to us case me hum pehle reverse of s nikalenge 
 //  Ab humare pass S and S' hai normal string aur reversed string
 //  Ab hum longest common subsequence find kar lenge S and S' k beech aur woi answer ho jaayega 
+
+
+// P.S.: Same logic will not valid to find longest pallindromic substring.

842cc46bd56769a0b8455c7d60c2ecc5cec4619d
Adarsh Dubey <75479397+dubeyadarsh@users.noreply.github.com>
2022-07-11 15:02:35 +0530

    Update MCM.java

--- DP2/Partition Dp/MCM.java
+++ DP2/Partition Dp/MCM.java
@@ -5,3 +5,5 @@
 
 // So question me hume bunch of matrix given hai aur unko multiply karna hai in a way such that number of operation minimum ho 
 // to hum chahe pehle last k 2 matrix multiply kare ya beech k 2 ya start k 2 matrix, har baar number of operation alag hoga .
+
+// one more things multiplication of matrix is possible if and only if number of col in 1st matrix should be equal to number of row in 2nd matrix

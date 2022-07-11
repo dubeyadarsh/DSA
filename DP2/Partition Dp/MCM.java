e67e32fece217c0b65d0726f3f728b78357633d3
Adarsh Dubey <75479397+dubeyadarsh@users.noreply.github.com>
2022-07-11 14:56:11 +0530

    Create MCM.java

---
+++ DP2/Partition Dp/MCM.java
@@ -0,0 +1,7 @@
+// In this type of problem
+// hum har index pe partition karte hau aur left and right half me divide karke solve karte hai
+// jis bhi partition per optimised answer aata hai hum wo return karte hai
+// for e.g, Matrix chain multiplication me number of operation = n*m*p hota hai jahan pe n*m is size of 1st matrix and m*p is the size of 2nd matrix.
+
+// So question me hume bunch of matrix given hai aur unko multiply karna hai in a way such that number of operation minimum ho 
+// to hum chahe pehle last k 2 matrix multiply kare ya beech k 2 ya start k 2 matrix, har baar number of operation alag hoga .
